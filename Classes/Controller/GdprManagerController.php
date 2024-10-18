78eab07ffcabb189f57d1a453837b955abdab6ef
gdpr-extensions-com <support@gdpr-extensions.com>
2024-10-18 11:22:57 +0500

    Prepare for stable release 1.0.2

--- Classes/Controller/GdprManagerController.php
+++ Classes/Controller/GdprManagerController.php
@@ -119,6 +119,7 @@ class GdprManagerController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionCont
                     'content' => '', // Default empty string
                     'button_text' => '', // Default empty string
                     'enable_background_image' => 0, // Default 0
+                    'heading_color' => '', // Default empty string
                     'background_image' => '', // Default empty string
                     'background_image_color' => '', // Default empty string
                     'button_color' => '', // Default empty string
--- Classes/Domain/Model/GdprManager.php
+++ Classes/Domain/Model/GdprManager.php
@@ -26,6 +26,13 @@ class GdprManager extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
      * @var string
      */
     protected $heading = '';
+    
+    /**
+     * headingColor
+     *
+     * @var string
+     */
+    protected $headingColor = '';
 
     /**
      * content
@@ -228,6 +235,27 @@ class GdprManager extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity
     public function setHeading(string $heading)
     {
         $this->heading = $heading;
+    }
+
+    /**
+     * Returns the headingColor
+     *
+     * @return string
+     */
+    public function getHeadingColor()
+    {
+        return $this->headingColor;
+    }
+
+    /**
+     * Sets the headingColor
+     *
+     * @param string $headingColor
+     * @return void
+     */
+    public function setHeadingColor(string $headingColor)
+    {
+        $this->headingColor = $headingColor;
     }
 
     /**
